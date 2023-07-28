96a69b2922d7d9e2660143568d0a0e3c5e56d7e1
MShahzodbek <mshahzodbek0309@gmail.com>
2023-07-28 14:41:43 +0500

    Added PostMapping for "/blog/add" url

--- src/main/java/com/example/springblogsite/controllers/BlogController.java
+++ src/main/java/com/example/springblogsite/controllers/BlogController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
@@ -21,8 +22,15 @@ public class BlogController {
         return "blog";
     }
 
-    @PostMapping("/blog/add")
+    @GetMapping("/blog/add")
     public String addBlog(Model model){
         return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title,@RequestParam String anons,@RequestParam String full_text, Model model){
+        Post post=new Post(title,anons,full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
     }
 }
